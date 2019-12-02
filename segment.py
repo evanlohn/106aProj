9240bbff13da2bd26f82bcbfc5cf10cf8596c58e
Rebecca <rebeccaabraham@berkeley.edu>
2019-12-01 21:07:00 -0800

    deleted trash imgs

--- segment.py
+++ segment.py
@@ -1,15 +1,17 @@
 import os
-import cv2
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
 import numpy as np
 
 #NOTE: img should be grayscale. see: 
 #https://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html?highlight=adaptivethreshold
-adap_types = {'mean': cv2.ADAPTIVE_THRESH_MEAN_C, 'gaussian': cv2.ADAPTIVE_THRESH_GAUSSIAN_C}
+adap_types = {'mean': cv.ADAPTIVE_THRESH_MEAN_C, 'gaussian': cv.ADAPTIVE_THRESH_GAUSSIAN_C}
 def segment_adaptive(img, adap_type='mean', blockSize=11, C=2):
-	first_thresh = cv2.adaptiveThreshold(img, 255, adap_types[adap_type], cv2.THRESH_BINARY, blockSize, C)
+	first_thresh = cv.adaptiveThreshold(img, 255, adap_types[adap_type], cv.THRESH_BINARY, blockSize, C)
 
 	connectivity = 8
-	num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(first_thresh , connectivity , cv2.CV_32S)
+	num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(first_thresh , connectivity , cv.CV_32S)
 
 	print(num_labels)
 	print(labels[:5])
@@ -20,7 +22,7 @@ def segment_adaptive(img, adap_type='mean', blockSize=11, C=2):
 def segment_reference(ref_img):
 	print('segmenting reference img')
 
-	gray = cv2.cvtColor(ref_img, cv2.COLOR_BGR2GRAY)
+	gray = cv.cvtColor(ref_img, cv.COLOR_BGR2GRAY)
 	#calc mask
 	#print(stats(gray))
 	blockSize= 501
@@ -28,11 +30,29 @@ def segment_reference(ref_img):
 
 	adap_mean = segment_adaptive(gray, adap_type='mean', blockSize=blockSize, C=C)
 	adap_gauss = segment_adaptive(gray, adap_type='gaussian', blockSize=blockSize, C=C)
+	
+	img = cv.imread('./raw_img_data/puzzle_pieces.png',0)
+	img = cv.medianBlur(img,5)
 
-	cv2.imshow('image', adap_mean)
-	cv2.waitKey()
-	cv2.imshow('image', adap_gauss)
-	cv2.waitKey()
+	ret,th1 = cv.threshold(gray,127,255,cv.THRESH_BINARY)
+	th2 = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,11,2)
+	th3 = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
+
+	titles = ['Original Image', 'Global Thresholding (v = 127)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+	images = [img, th1, th2, th3]
+
+	contours, hierarchy = cv.findContours(th2, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+	cv.drawContours(img, contours, -1, (0,255,0), 3)
+
+	for i in range(4):
+	    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+	    plt.title(titles[i])
+	    plt.xticks([]),plt.yticks([])
+	plt.show()
+
+
+
 
 
 def stats(img):
@@ -45,7 +65,7 @@ def stats(img):
 
 
 def main():
-	ref_img = cv2.imread('./reference_data/img0.png')
+	ref_img = cv.imread('./raw_img_data/puzzle_pieces.png')
 	segment_reference(ref_img)
 
 
