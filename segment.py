dd147a99aebeab16f104d6a90e1f36e350d43230
Jason Huynh <jasonhuynh@Jasons-MacBook.local>
2019-12-07 17:49:02 -0800

    can now deskew images

--- segment.py
+++ segment.py
@@ -1,11 +1,10 @@
-import os
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-import numpy as np
 import argparse
+import math
 
-#NOTE: img should be grayscale. see: 
+#NOTE: img should be grayscale. see:
 #https://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html?highlight=adaptivethreshold
 adap_types = {'mean': cv.ADAPTIVE_THRESH_MEAN_C, 'gaussian': cv.ADAPTIVE_THRESH_GAUSSIAN_C}
 def segment_adaptive(img, adap_type='mean', blockSize=11, C=2):
@@ -46,7 +45,7 @@ def segment_reference(ref_img, method):
 		    plt.xticks([]),plt.yticks([])
 		plt.show()
 	else:
-	
+
 		img = cv.imread('./raw_img_data/puzzle_pieces.png',0)
 		img = cv.medianBlur(img,5)
 
@@ -80,7 +79,6 @@ def stats(img):
 def main():
 	parser = argparse.ArgumentParser(description='specify which file(s) to segment')
 	parser.add_argument('file', type=str, nargs='?', default='./raw_img_data/puzzle_pieces.png')
-
 	args = parser.parse_args()
 	ref_img = cv.imread(args.file)
 	segment_reference(ref_img, 'evan')
